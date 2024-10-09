5e7c1725643c855e92d83c65977f8afb9061df49
smkn3django <smk3django@gmail.com>
2024-10-09 10:02:58 +0800

    update sintax get total files in folder

--- scanmonitoring/management/commands/monitor_folder.py
+++ scanmonitoring/management/commands/monitor_folder.py
@@ -8,26 +8,27 @@ class Command(BaseCommand):
     help = 'Monitors a folder for new files and logs total files to the database every 5 seconds'
 
     def handle(self, *args, **options):
-        source_path = 'c://dms'  # Set your folder path here
-        source_uncategories = 'c://dms'
-        source_large_file= 'c://dms'
-        source_miss_nip = 'c://dms'
-        source_not_a_pdf = 'c://dms'
-        source_doc_proceed = 'c://dms'
-        source_doc_uploaded ='c://dms'
+        source_path = 'Z:\\SMKN3 MANADO'  # Set your folder path here
+        source_uncategories = 'Z:\\DOCUMENT_REJECTION\\UNCATEGORIES'
+        source_large_file= 'Z:\\DOCUMENT_REJECTION\\LARGE'
+        source_miss_nip = 'Z:\\DOCUMENT_REJECTION\\MISS_NIP'
+        source_not_a_pdf = 'Z:\\DOCUMENT_REJECTION\\NOT_A_PDF'
+        source_doc_proceed = 'Z:\\DOCUMENT_APPROVED'
+        source_doc_uploaded ='Z:\\DOCUMENT_APPROVED'
 
         self.stdout.write(self.style.SUCCESS(f'Starting to monitor folder: {source_path}'))
 
         while True:
             # Count total files in the folder
-            total_files = len([f for f in os.listdir(source_path) if os.path.isfile(os.path.join(source_path, f))])
+            # total_files = len([f for f in os.listdir(source_path) if os.path.isfile(os.path.join(source_path, f))])
+            total_files = sum([len(files) for _, _, files in os.walk(source_path)])
             
-            total_uncategories = len([f for f in os.listdir(source_uncategories) if os.path.isfile(os.path.join(source_uncategories, f))])
-            total_large_file= len([f for f in os.listdir(source_large_file) if os.path.isfile(os.path.join(source_large_file, f))])
-            total_miss_nip = len([f for f in os.listdir(source_miss_nip) if os.path.isfile(os.path.join(source_miss_nip, f))])
-            total_not_a_pdf = len([f for f in os.listdir(source_not_a_pdf) if os.path.isfile(os.path.join(source_not_a_pdf, f))])
-            total_doc_proceed = len([f for f in os.listdir(source_doc_proceed) if os.path.isfile(os.path.join(source_doc_proceed, f))])
-            total_doc_uploaded = len([f for f in os.listdir(source_doc_uploaded) if os.path.isfile(os.path.join(source_doc_uploaded, f))])
+            total_uncategories = sum([len(files) for _, _, files in os.walk(source_uncategories)])
+            total_large_file= sum([len(files) for _, _, files in os.walk(source_large_file)])
+            total_miss_nip = sum([len(files) for _, _, files in os.walk(source_miss_nip)])
+            total_not_a_pdf = sum([len(files) for _, _, files in os.walk(source_not_a_pdf)])
+            total_doc_proceed = sum([len(files) for _, _, files in os.walk(source_doc_proceed)])
+            total_doc_uploaded = sum([len(files) for _, _, files in os.walk(source_doc_uploaded)])
 
             # Log the total files and the current time to the database
             MonitoringLog.objects.create(total_files=total_files, total_uncategories = total_uncategories,
